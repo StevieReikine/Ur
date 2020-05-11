2a2ba130d020f725be313b5b7f05546c7ee33dde
sr722 <sr722@moore.local>
2020-05-11 22:51:27 +0100

    version 1 of code to actually play the game in terminal- almost works ;-)

---
+++ play_ur.py
@@ -0,0 +1,50 @@
+from board import PrintBoard, EmptyBoard, Winner, CheckMovePossible, player_input, get_index, UpdateV, UpdateBoard
+from die_roll import Dice
+
+#initial set up of parameters
+board = EmptyBoard()
+v1 = [0]*14     #for player 1, initialize to zeros
+v2 = [0]*14     #for player 2, initialize to zeros
+off_p1 = 0
+off_p2 = 0
+new_piece = False
+game_over = False
+player = [1 , 2]
+player1 = player[0]
+player2 = player[1]
+p = 0
+
+while not game_over:
+    # show the board
+    PrintBoard(board)
+    # print player's turn
+    print("Player ", player[p])
+    # roll the dice
+    dice = Dice()
+    dice.roll()
+    dice.show_roll()
+    roll = dice.result_roll()
+    # check if move possible
+    check = CheckMovePossible(v1, v2, roll, player[p])
+    if check: 
+        row, pos = player_input()
+        index, new_piece = get_index(row, pos)
+        print('index: ', index, 'new_piece: ', new_piece)
+        v1, v2, off_p1, off_p2, is_rosette = UpdateV(v1, v2, player[p], roll, index, off_p1, off_p2, new_piece)
+        print('v1: ', v1, 'v2: ', v2, 'off_p1: ', off_p1, 'off_p2: ', off_p2, 'is_rosette: ', is_rosette)
+        board = UpdateBoard(v1, v2)
+        if is_rosette:
+            p = p
+        else:
+            if p == 0:
+                p = 1
+            else:
+                p = 0
+        game_over = Winner(off_p1, off_p2)
+    else:
+        print('No valid move. :( ')
+        if p == 0:
+            p = 1
+        else:
+            p = 0
+
