4c2059b2da833314b5bd26218118509247123ac6
sr722 <sr722@moore.local>
2020-05-14 14:50:34 +0100

    updated check functions and player selection

--- play_ur.py
+++ play_ur.py
@@ -1,5 +1,6 @@
-from board import PrintBoard, EmptyBoard, Winner, CheckMovePossible, player_input, get_index, UpdateV, UpdateBoard
+from board import PrintBoard, EmptyBoard, Winner, CheckMovePossible, CheckMove, player_input, get_index, UpdateV, UpdateBoard
 from die_roll import Dice
+from itertools import cycle
 
 #initial set up of parameters
 board = EmptyBoard()
@@ -7,12 +8,20 @@ v1 = [0]*14     #for player 1, initialize to zeros
 v2 = [0]*14     #for player 2, initialize to zeros
 off_p1 = 0
 off_p2 = 0
+new_p1 = 7
+new_p2 = 7
 new_piece = False
 game_over = False
 player = [1 , 2]
-player1 = player[0]
-player2 = player[1]
 p = 0
+
+def NextPlayer(p):
+    if p == 0:
+        p = 1
+    else:
+        p = 0
+    return p
+    
 
 while not game_over:
     # show the board
@@ -25,25 +34,21 @@ while not game_over:
     dice.show_roll()
     roll = dice.result_roll()
     # check if move possible
-    
-    check = CheckMovePossible(v1, v2, roll, player[p])
+    check = CheckMovePossible(v1, v2, roll, player[p],new_p1, new_p2)
     if check: 
         row, pos = player_input()
         index, new_piece = get_index(row, pos)
-        v1, v2, off_p1, off_p2, is_rosette = UpdateV(v1, v2, player[p], roll, index, off_p1, off_p2, new_piece)
-        board = UpdateBoard(v1, v2)
-        if is_rosette:
-            p = p
+        check = CheckMove(v1, v2, player[p], roll, index, new_p1, new_p2)
+        if check == False:
+            print("This is not a valid move. ")
+            p = NextPlayer(p)
         else:
-            if p == 0:
-                p = 1
-            else:
-                p = 0
-        game_over = Winner(off_p1, off_p2)
+            v1, v2, new_p1, new_p2, off_p1, off_p2, is_rosette = UpdateV(v1, v2, player[p], roll, index, new_p1, new_p2, off_p1, off_p2, new_piece)
+            board = UpdateBoard(v1, v2)
+            if is_rosette == False: 
+                p = NextPlayer(p)
+            game_over = Winner(off_p1, off_p2)
     else:
         print('No valid move. :( ')
-        if p == 0:
-            p = 1
-        else:
-            p = 0
+        p = NextPlayer(p)
 
